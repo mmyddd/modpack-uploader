abc251f9b30ebe0edf8c521a01a3237cebf2b0f4
DE_SHARK <2799045359@qq.com>
2025-06-13 23:05:29 +0800

    release: 1.0.0

--- src/main/java/com/deshark/core/schemas/Config.java
+++ src/main/java/com/deshark/core/schemas/Config.java
@@ -8,6 +8,6 @@ public record Config(
         String versionName
 ) {
     public static Config empty() {
-        return new Config(StorageConfig.empty(), "", "", "", "");
+        return new Config(StorageConfig.empty(), "source", "source-server", "source-client", "1.0.0");
     }
 }
